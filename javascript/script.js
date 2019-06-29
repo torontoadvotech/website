6a8094f312ee40070bb272d2667a315399ce4c58
Dennis <dennis.mcleod92@gmail.com>
2019-06-28 20:27:36 -0400

    header styles and transitions

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+$(function() {
+  const shrinkNav = () => {
+    const scrollDistance = $(document).scrollTop();
+
+    if (scrollDistance > 100) {
+      $('header').addClass('scrolled-header');
+      $('.to').css('font-size', '3.5rem');
+      $('.wit').css('font-size', '1.3rem');
+    } else {
+      $('header').removeClass('scrolled-header');
+      $('.to').css('font-size', '5rem');
+      $('.wit').css('font-size', '1.7rem');
+    }
+  };
+
+  $(document).on('scroll', shrinkNav);
+});
